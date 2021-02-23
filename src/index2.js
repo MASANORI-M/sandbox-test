ab22fb92046e92dd29799373bf6d35bf12e9e821
MASANORI-M <77526005+MASANORI-M@users.noreply.github.com>
2021-02-23 23:23:41 +0900

    UP2

---
+++ src/index2.js
@@ -0,0 +1,12 @@
+// map filter
+
+const nameArr = ["田中", "山田", "鈴木"];
+
+const nameArr2 = nameArr.map((name) => {
+  return name;
+});
+console.log(nameArr2);
+
+// 三項演算子
+const val1 = 1 > 0 ? "trueです" : "falseです";
+console.log(val1);
